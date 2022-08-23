568e62f71ba103286b79af25a99cb878e80ed7d8
marcocalligaris <marcocally@gmail.com>
2022-08-23 19:14:46 +0200

    Added Header component

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,22 @@
 <template>
-  <div class="container">
-    
+  <div>
+    <HeaderItem />
   </div>
 </template>
 
 <script>
+import HeaderItem from './components/HeaderItem.vue';
 
 export default {
-  name: 'App',
+  name: "App",
+  components: { HeaderItem },
+  data() {
+    return {
+      headerMenu:[
+        {id:1, url:'#', text:'CHARACTERS', active: false},
+        ],
+    }
+  },
 }
 </script>
 
