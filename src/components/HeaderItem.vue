8fb2fc600c8b771513c50fcd51d15a670690e63f
marcocalligaris <marcocally@gmail.com>
2022-08-23 19:14:10 +0200

    Added Header item and jumbotron

---
+++ src/components/HeaderItem.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="jumbotron">
+    
+  </div>
+</template>
+
+<script>
+export default {
+name: 'HeaderItem',
+}
+</script>
+
+<style lang="scss" scoped>
+@import '../assets/scss/style.scss';
+#jumbotron {
+  position: relative;
+  overflow: hidden;
+  background-color: $jumbotron;
+  &::before {
+    content: "";
+    position: absolute;
+    width: 100%;
+    height: 50%;
+    bottom: 0;
+    background-image: url('../assets/img/jumbo-overlay.svg');
+    transform: rotateX(180deg);
+  };
+}
+</style>
