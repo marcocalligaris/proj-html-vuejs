c7dd03d8d3bd06fe37f61b6dc4f5a12085c2f456
marcocalligaris <marcocally@gmail.com>
2022-08-24 08:29:43 +0200

    Added header dynamic menu

--- src/components/HeaderItem.vue
+++ src/components/HeaderItem.vue
@@ -4,6 +4,18 @@
     <div class="container">
       <header class="py-3 d-flex justify-content-between align-items-center">
         <a href="#"><img id="logo" src="../assets/img/dark-logo.png" alt="Max Coach"></a>
+        <nav class="d-flex">
+          <ul class="d-flex">
+            <li v-for="link in links" :key="link.id"
+            class="me-5 fw-semibold">
+            <a 
+            :href="link.url"
+            >{{link.text}} <i class="fa-solid fa-angle-down"></i>
+            </a>
+            </li>
+          </ul>
+
+        </nav>
       </header>
 
     </div>
@@ -13,6 +25,9 @@
 <script>
 export default {
 name: 'HeaderItem',
+props: {
+  links: Array,
+  },
 }
 </script>
 
@@ -31,8 +46,18 @@ name: 'HeaderItem',
     background-image: url('../assets/img/jumbo-overlay.svg');
     transform: rotateX(180deg);
   };
-  header #logo {
-    width: 150px;
+  header {
+    height: 100px;
+    line-height: 100px;
+    #logo {
+      width: 150px;
+    }
+    ul {
+      list-style-type: none;
+      a {
+        color: $titles;
+      }
+    }
   }
 }
 </style>
