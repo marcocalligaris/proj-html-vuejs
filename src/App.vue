68693a5efbe9027ad8b8322992e6b79528b534a3
marcocalligaris <marcocally@gmail.com>
2022-08-25 16:47:05 +0200

    Added address info

--- src/App.vue
+++ src/App.vue
@@ -192,6 +192,29 @@
         </div>
       </section>
     </main>
+    <footer>
+      <div class="container">
+        <div class="row">
+          <div class="col-6">
+            <address class="mb-5">
+              <h5 class="fw-bold mb-3">Address</h5>
+              <div class="text-secondary fw-semibold">
+                <p>382 NE 191st St # 87394 Miami, FL 33179-3899</p>
+                <p>+1 (305) 547-9909 (9am - 5pm EST, Monday - Friday)</p>
+                <p>support@maxcoach.com</p>
+              </div>
+            </address>
+            <ul class="d-flex">
+              <li class="me-4 text-secondary"><a href="#"></a><i class="fa-brands fa-facebook"></i></li>
+              <li class="me-4 text-secondary"><a href="#"></a><i class="fa-brands fa-twitter"></i></li>
+              <li class="me-4 text-secondary"><a href="#"></a><i class="fa-brands fa-instagram"></i></li>
+              <li class="me-4 text-secondary"><a href="#"></a><i class="fa-brands fa-linkedin"></i></li>
+            </ul>
+          </div>
+          <div class="col-6"></div>
+        </div>
+      </div>
+    </footer>
   </div>
 </template>
 <script>
@@ -546,5 +569,10 @@ ul {
       };
     };
   };
+};
+address {
+  p {
+    font-size: 13px;
+  }
 }
 </style>
